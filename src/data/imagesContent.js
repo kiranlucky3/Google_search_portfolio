c449ae973bd288d8f81a46b05b1a0eedc594b9b8
Vishnu Kiran Dabbara <83537653+kiranlucky3@users.noreply.github.com>
2022-05-01 21:49:01 +0200

    Update imagesContent.js

--- src/data/imagesContent.js
+++ src/data/imagesContent.js
@@ -11,69 +11,41 @@ import gh from "../images/github-icon.png";
 import email from "../images/gmail-icon.png";
 import placeholder from "../images/bg1.png";
 export const ImagesContent = [
-  {
-    link: "https://github.com/enjeck/TutCode",
-    img: tutcode,
-    name: "TutCode - site to simultaneously  view tutorials and code",
-  },
+
   {
     link: `${window.location.origin}`,
     img: gfolio,
     name: "Google Search Potfolio (this website)",
   },
   {
-    link: "https://enjeck.com/",
-    img: enjeck,
-    name: "Personal Website",
+    link: "http://kiran-register-login.s3-website-us-east-1.amazonaws.com/",
+    img: aws-website,
+    name: "aws-website",
   },
   {
-    link: "https://github.com/enjeck/libre-logos",
-    img: ll,
-    name: "Libre Logos",
+    link: "https://github.com/kiranlucky3/Crypto_Hunter/tree/master",
+    img: Crypto-Hunter,
+    name: "Crypto-hunter",
   },
   {
-    link: "https://github.com/enjeck/jpg2svg",
-    img: jpg2svg,
-    name: "jpg2svg",
+    link: "https://github.com/kiranlucky3/Brick_Breaker",
+    img:brick-breaker,
+    name:"brick breaker"
   },
   {
-    link: "https://github.com/enjeck/CrazyPassword",
-    img: placeholder,
-    name: "Crazy Password",
+    link: "https://github.com/kiranlucky3/Ship_of_Fools",
+    img:Python-logo,
+    name:"Python-logo"
   },
   {
-    link: "https://github.com/enjeck/Geo-Guess",
-    img: gg,
-    name: "Geo Guess",
-  },
-  {
-    link: "https://github.com/enjeck/Blobby",
-    img: blobby,
-    name: "Blobby",
-  },
-  {
-    link: "https://github.com/enjeck/Google-Sheet-to-website",
-    img: placeholder,
-    name: "Google Sheet to Website",
-  },
-  {
-    link: "https://github.com/enjeck/CryptoAlgoVisualizer",
-    img: cav,
-    name: "Crypto Algorithm Visualizer",
-  },
-  {
-    link: "https://github.com/enjeck/btns",
-    img: placeholder,
-    name: "btns",
-  },
-  {
-    link: "https://github.com/enjeck",
+    link: "https://github.com/kiranlucky3",
     img: gh,
     name: "GitHub - Explore my projects and code"
   },
   {
-    link: "https://www.linkedin.com/in/c1e0/",
+    link: "https://www.linkedin.com/in/vishnukirandabbara/",
     img: li,
     name: "LinkedIn - Connect and share experiences"
   },
+  
 ];
